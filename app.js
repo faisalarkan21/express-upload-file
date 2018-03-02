fa974a06cf2528d59ae93e63401045c9fef6bb95
faisal arkan <faisalarkan21@gmail.com>
2018-03-02 16:33:29 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const multer = require('multer');
+const crypto = require('crypto');
+
+var storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+
+const upload = multer({
+  dest: 'uploads/', // this saves your file into a directory called "uploads"
+  storage: storage 
+
+
+}); 
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// // view engine setup
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+// It's very crucial that the file name matches the name attribute in your html
+app.post('/', upload.single('fileUpload'), (req, res) => {
+  console.log(req.file);
+  res.redirect('/');
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
